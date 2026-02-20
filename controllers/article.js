520536c29abb8bb35a327b600b1aa44571495694
Tanel Pauls <paulstanel@protonmail.com>
2026-02-20 13:17:15 +0200

    Get article author data

--- controllers/article.js
+++ controllers/article.js
@@ -9,22 +9,24 @@ const sequelize = new Sequelize(
     dialect: "mysql"
   }
 );
-
-const Article = require('../models/article.js')(sequelize, Sequelize.DataTypes);
+const models = require('../models');
 
 const getAllArticles = (req, res) => {
-    Article.findAll().then(articles => {
-        return res.status(200).json({articles});
-    }).catch(error => {
-        return res.status(500).send(error.message);
-    })
+  models.Article.findAll().then(articles => {
+      return res.status(200).json({articles});
+  }).catch(error => {
+      return res.status(500).send(error.message);
+  })
 }
 
 const getArticleBySlug = (req, res) => {
-  Article.findOne({
+  models.Article.findOne({
     where: {
       slug: req.params.slug
-    }
+    },
+    include: [{
+      model: models.Authors
+    }],
   }).then(article => {
     return res.status(200).json({article})
   }).catch(error=> {
