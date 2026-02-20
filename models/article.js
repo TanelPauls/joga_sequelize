59eab881524ed50c20b3ae27e3faf49d9baf8699
Tanel Pauls <paulstanel@protonmail.com>
2026-02-20 10:48:49 +0200

    Create article table and added table data

---
+++ models/article.js
@@ -0,0 +1,51 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Article extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Article.init({
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false
+    },
+    name: {
+      type: DataTypes.SRING,
+      allowNull: false
+    },
+    slug: {
+      type: DataTypes.SRING,
+      allowNull: false,
+      unique: true
+    },
+    image: {
+      type: DataTypes.SRING,
+      allowNull: false
+    },
+    body: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    published: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    author_id: DataTypes.INTEGER
+
+  }, {
+    sequelize,
+    modelName: 'Article',
+  });
+  return Article;
+};
